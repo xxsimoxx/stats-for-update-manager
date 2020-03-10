291f7a1d6c38001b707bc1d0a5fb5be634edad1c
xxsimoxx <simospa@gieffeedizioni.it>
2020-03-10 13:35:55 +0100

    strict checks

--- classes/ListTable.class.php
+++ classes/ListTable.class.php
@@ -62,9 +62,9 @@ class SFUM_List_Table extends \WP_List_Table {
 	// Callable to be used with usort.
 	function reorder( $a, $b ) {
 		// If no orderby or wrong orderby, default to plugin or theme name.
-		$orderby = (!empty($_GET['orderby']) && in_array($_GET['orderby'], ['name', 'count', 'type'])) ? $_GET['orderby'] : 'name';
+		$orderby = (!empty($_GET['orderby']) && in_array($_GET['orderby'], ['name', 'count', 'type'], true)) ? $_GET['orderby'] : 'name';
 		// If no order or wrong order, default to asc.
-		$order = (!empty($_GET['order']) && in_array($_GET['order'], ['asc', 'desc'])) ? $_GET['order'] : 'asc';
+		$order = (!empty($_GET['order']) && $_GET['order'] !== 'asc') ? 'desc' : 'asc';
 
 		$result = strcasecmp( $a[$orderby], $b[$orderby] );
 
