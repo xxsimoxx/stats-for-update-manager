84cb7dbbde50115ebf58b4cb1b0e3cb998b6ac99
xxsimoxx <simospa@gieffeedizioni.it>
2020-03-16 11:21:28 +0100

    Fixed wrong sorting in plgin count

--- stats-for-update-manager.php
+++ stats-for-update-manager.php
@@ -375,7 +375,7 @@ class StatsForUpdateManager{
 				'identifier' => $value->slug,
 				'name'       => get_the_title($um_posts[$value->slug]),
 				'id'         => $um_posts[$value->slug],
-				'count'      => $value->total,
+				'count'      => (int)$value->total,
 				'type'       => $this->is_theme($um_posts[$value->slug]) ? esc_html__('Theme', 'stats-for-update-manager') : esc_html__('Plugin', 'stats-for-update-manager'),
 			];
 		}
