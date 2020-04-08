e269d3b441b98f5a64eec3246a034ee56d449e9a
xxsimoxx <simospa@gieffeedizioni.it>
2020-04-08 17:43:48 +0200

    More logical function order in class

--- classes/ListTable.class.php
+++ classes/ListTable.class.php
@@ -22,11 +22,25 @@ if (!class_exists('WP_List_Table')) {
 // Class for display statistics page.
 class SFUM_List_Table extends \WP_List_Table {
 
+	// Contains the requested filter type (all, plugins or themes).
+	private $filtertype = 'all';
+
 	// Contains the data to be rendered, as we want this to be passed from another class.
 	private $data = [];
 
-	// Contains the requested filter type (all, plugins or themes).
-	private $filtertype = 'all';
+	// Load list items, as we want this to be passed from another class.
+	public function load_items($statistics) {
+		$this->data = $statistics;
+	}
+
+	// Parse request to understand what to filter for.
+	function get_filtertype() {
+		// Sanitize filtertype and default to all
+		if (empty($_GET['filtertype']) || !in_array($_GET['filtertype'], ['all', 'plugins', 'themes'], true)) {
+			return 'all';
+		}
+		return $_GET['filtertype'];
+	}
 
 	// Output columns definition.
 	function get_columns() {
@@ -40,16 +54,6 @@ class SFUM_List_Table extends \WP_List_Table {
 		return $columns;
 	}
 
-	// Output sortable columns.
-	function get_sortable_columns() {
-		$sortable_columns = [
-			'name'    => ['name', false],
-			'count'   => ['count', false],
-			'type'    => ['type', false],
-		];
-		return $sortable_columns;
-	}
-
 	// Output hidden columns.
 	function get_hidden_columns() {
 		$hidden_columns = [
@@ -59,36 +63,14 @@ class SFUM_List_Table extends \WP_List_Table {
 		return $hidden_columns;
 	}
 
-	// Callable to be used with usort.
-	function reorder($a, $b) {
-		// If no orderby or wrong orderby, default to plugin or theme name.
-		$orderby = (!empty($_GET['orderby']) && in_array($_GET['orderby'], ['name', 'count', 'type'], true)) ? $_GET['orderby'] : 'name';
-		// If no order or wrong order, default to asc.
-		$order = (!empty($_GET['order']) && $_GET['order'] !== 'asc') ? 'desc' : 'asc';
-
-		// Properly order numeric values.
-		if (is_int($a[$orderby])) {
-			$result = $a[$orderby] - $b[$orderby];
-		} else {
-			$result = strcasecmp($a[$orderby], $b[$orderby]);
-		}
-
-		return ( $order === 'asc' ) ? $result : -$result;
-	}
-
-	// Just output the column.
-	function column_default($item, $column_name) {
-		return $item[$column_name];
-	}
-
-	// For "Name" column add row action and reformat it.
-	function column_name($item) {
-		$actions = [
-			'edit'  => '<a href="'.admin_url('post.php?post='.$item['id'].'&action=edit">'.esc_html__('Edit', 'stats-for-update-manager').'</a>'),
-			'delete' => '<a href="'.wp_nonce_url(home_url(add_query_arg(['action' => 'delete', 'id' => $item['id']])), 'delete', '_sfum').'">'.esc_html__('Reset', 'stats-for-update-manager').'</a>',
+	// Output sortable columns.
+	function get_sortable_columns() {
+		$sortable_columns = [
+			'name'    => ['name', false],
+			'count'   => ['count', false],
+			'type'    => ['type', false],
 		];
-		$name = '<span class="row-title">'.$item['name'].'</span>';
-		return sprintf('%1$s %2$s', $name, $this->row_actions($actions));
+		return $sortable_columns;
 	}
 
 	// Deal with "reset" action.
@@ -130,28 +112,11 @@ class SFUM_List_Table extends \WP_List_Table {
 		// Translators: %1$s is plugin or theme name.
 		echo '<div class="notice notice-success is-dismissible"><p>'.sprintf(__('Statistics for %1$s has been successfully reset.', 'stats-for-update-manager'), $name).'</p></div>';
 
-		// Remove action from query args.
+		// Remove delete action from query args.
 		echo '<script>';
 		echo 'window.history.pushState({}, document.title, "'.remove_query_arg(['action', 'id', '_sfum']).'");';
 		echo '</script>';
 
-	}
-
-	// Display filter for plugins or themes.
-	function extra_tablenav($which) {
-		$theme_count = $this->get_theme_count();
-		$all_count = count($this->data);
-		$plugin_count = $all_count - $theme_count;
-		echo '<ul class="subsubsub">';
-		echo '<li><a '.($this->filtertype === 'all' ? 'class="current"' : '').' href="'.home_url(add_query_arg('filtertype', 'all')).'">'.esc_html__('All', 'stats-for-update-manager').'<span class="count"> ('.$all_count.')</span></a> |</li>';
-		echo '<li><a '.($this->filtertype === 'plugins' ? 'class="current"' : '').' href="'.home_url(add_query_arg('filtertype', 'plugins')).'">'.esc_html__('Plugins', 'stats-for-update-manager').'<span class="count"> ('.$plugin_count.')</span></a> |</li>';
-		echo '<li><a '.($this->filtertype === 'themes' ? 'class="current"' : '').' href="'.home_url(add_query_arg('filtertype', 'themes')).'">'.esc_html__('Themes', 'stats-for-update-manager').'<span class="count"> ('.$theme_count.')</span></a></li>';
-		echo '</ul>';
-	}
-
-	// Load list items, as we want this to be passed from another class.
-	function load_items($statistics) {
-		$this->data = $statistics;
 	}
 
 	// Filter plugin or themes for display.
@@ -169,6 +134,50 @@ class SFUM_List_Table extends \WP_List_Table {
 		return $list;
 	}
 
+	// Callable to be used with usort.
+	function reorder($a, $b) {
+		// If no orderby or wrong orderby, default to plugin or theme name.
+		$orderby = (!empty($_GET['orderby']) && in_array($_GET['orderby'], ['name', 'count', 'type'], true)) ? $_GET['orderby'] : 'name';
+		// If no order or wrong order, default to asc.
+		$order = (!empty($_GET['order']) && $_GET['order'] !== 'asc') ? 'desc' : 'asc';
+
+		// Properly order numeric values or reorder text case-insensitive.
+		if (is_int($a[$orderby])) {
+			$result = $a[$orderby] - $b[$orderby];
+		} else {
+			$result = strcasecmp($a[$orderby], $b[$orderby]);
+		}
+
+		return ( $order === 'asc' ) ? $result : -$result;
+	}
+
+	// Just output the column.
+	function column_default($item, $column_name) {
+		return $item[$column_name];
+	}
+
+	// For "Name" column add row action and reformat it.
+	function column_name($item) {
+		$actions = [
+			'edit'  => '<a href="'.admin_url('post.php?post='.$item['id'].'&action=edit">'.esc_html__('Edit', 'stats-for-update-manager').'</a>'),
+			'delete' => '<a href="'.wp_nonce_url(home_url(add_query_arg(['action' => 'delete', 'id' => $item['id']])), 'delete', '_sfum').'">'.esc_html__('Reset', 'stats-for-update-manager').'</a>',
+		];
+		$name = '<span class="row-title">'.$item['name'].'</span>';
+		return sprintf('%1$s %2$s', $name, $this->row_actions($actions));
+	}
+
+	// Display filter for plugins or themes.
+	function extra_tablenav($which) {
+		$theme_count = $this->get_theme_count();
+		$all_count = count($this->data);
+		$plugin_count = $all_count - $theme_count;
+		echo '<ul class="subsubsub">';
+		echo '<li><a '.($this->filtertype === 'all' ? 'class="current"' : '').' href="'.home_url(add_query_arg('filtertype', 'all')).'">'.esc_html__('All', 'stats-for-update-manager').'<span class="count"> ('.$all_count.')</span></a> |</li>';
+		echo '<li><a '.($this->filtertype === 'plugins' ? 'class="current"' : '').' href="'.home_url(add_query_arg('filtertype', 'plugins')).'">'.esc_html__('Plugins', 'stats-for-update-manager').'<span class="count"> ('.$plugin_count.')</span></a> |</li>';
+		echo '<li><a '.($this->filtertype === 'themes' ? 'class="current"' : '').' href="'.home_url(add_query_arg('filtertype', 'themes')).'">'.esc_html__('Themes', 'stats-for-update-manager').'<span class="count"> ('.$theme_count.')</span></a></li>';
+		echo '</ul>';
+	}
+
 	// Retrieve the number of themes.
 	function get_theme_count() {
 		$count = 0;
@@ -178,15 +187,6 @@ class SFUM_List_Table extends \WP_List_Table {
 			}
 		}
 		return $count;
-	}
-
-	// Parse request to understand what to filter for.
-	function get_filtertype() {
-		// Sanitize filtertype and default to all
-		if (empty($_GET['filtertype']) || !in_array($_GET['filtertype'], ['all', 'plugins', 'themes'], true)) {
-			return 'all';
-		}
-		return $_GET['filtertype'];
 	}
 
 	// Prepare our columns and insert data.
