a11da00063fff5fcc9fb3189319e168f1e01effc
xxsimoxx <simospa@gieffeedizioni.it>
2025-03-27 15:05:55 +0100

    Fix remaining

--- uninstall.php
+++ uninstall.php
@@ -10,7 +10,7 @@ require_once('includes/constants.php');
 
 // Delete table.
 global $wpdb;
-$wpdb->query($wpdb->prepare('DROP TABLE IF EXISTS %s', $wpdb->prefix.DB_TABLE_NAME));
+$wpdb->query($wpdb->prepare('DROP TABLE IF EXISTS %s', $wpdb->prefix.DB_TABLE_NAME)); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange
 
 // Delete options.
 delete_option('sfum_db_ver');
